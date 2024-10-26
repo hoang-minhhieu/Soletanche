1f86cc9cb85700ab3a196076e05ad316a37b264e
mhoang20 <mhoang20@dxc.com>
2024-10-26 18:12:48 +0200

    Step 3 Check duplicate OK - Check dependency OK

--- src/app/step3/step3.component.ts
+++ src/app/step3/step3.component.ts
@@ -1,69 +1,103 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DetailedCellError, ExportedNamedExpressionChange, HyperFormula } from 'hyperformula';
 import { MatTable, MatTableModule } from '@angular/material/table';
 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { TableRow } from '../step1/step1.component';
 
 @Component({
   selector: 'app-step3',
   standalone: true,
-  imports: [MatTableModule, FormsModule, CommonModule],
+  imports: [MatTableModule, FormsModule, CommonModule, MatIconModule],
   templateUrl: './step3.component.html',
   styleUrl: './step3.component.css'
 })
-export class Step3Component {
-  displayedColumns: string[] = ['code', 'formule', 'valeur'];
-  sheetName = 'Step3';
-  sheetId!: number;
+export class Step3Component implements OnInit {
+  displayedColumns: string[] = ['code', 'formule', 'valeur', 'delete'];
+  data: TableRow[] = [];
   @ViewChild(MatTable) table!: MatTable<any>;
+  hf: HyperFormula = HyperFormula.buildEmpty({ licenseKey: 'gpl-v3' });
+  previousCodeValue: any = null; // Stocker la valeur de Code avant le trigger (change) au cas ou si la nouvelle valeur n'est pas valide
 
-  constructor() {
-    // this.hyperFormulaService = _hyperFormulaService;
-    // this.hyperFormulaService.addNewSheet(this.sheetName);
-    // const tmp = this.hyperFormulaService.getSheetId(this.sheetName);
-    // if (tmp !== null) {
-    //   this.sheetId = tmp;
-    // };
-    // this.data = [];
-    // this.tableDataSource = new MatTableDataSource(this.data);
+  constructor() { }
+
+  ngOnInit(): void {
+    this.data = []
   }
 
-  // onChangeCode(valeur: Event, idx: number) {
-  //   if (valeur.target !== null) {
-  //     const isValid = this.hyperFormulaService.isItPossibleToAddNamedExpression((valeur.target as HTMLInputElement).value, this.data[idx].formule);
-  //     if (isValid) {
-  //       this.hyperFormulaService.addNamedExpression((valeur.target as HTMLInputElement).value, this.data[idx].formule);
-  //     } else {
-  //       alert('Le même code existe déjà.');
-  //     }
-  //   }
-  // }
+  onChangeCode(event: Event, idx: number) {
+    const element = event.target;
+    if (element !== null) {
+      const newCode = (element as HTMLInputElement).value;
+      const formula = this.data[idx].formule;
+      const isValid = this.hf.isItPossibleToAddNamedExpression(newCode, formula);
+      if (isValid) {
+        this.hf.addNamedExpression(newCode, `=${formula}`);
+        if (this.previousCodeValue !== '') {
+          this.hf.removeNamedExpression(this.previousCodeValue);
+        }
+        this.previousCodeValue = newCode;
+      } else {
+        this.data[idx].code = this.previousCodeValue;
+        alert('Le même code existe déjà. Veuillez choisir un autre code');
+      }
+    }
+  }
 
-  // onChangeFormula(valeur: Event, idx: number) {
-  //   if (valeur.target !== null) {
-  //     const result = this.hyperFormulaService.verifyFormula((valeur.target as HTMLInputElement).value, idx, this.sheetId);
-  //     if (result !== undefined && result !== null) {
-  //       const cellChanged = this.hyperFormulaService.changeNamedExpression(`VAR_${idx + 1}`, Number(result));
-  //       if (cellChanged.length === 1) {
-  //         this.data[idx].valeur = Number(cellChanged[0].newValue);
-  //       } else {
-  //         for (let i = 0; i < cellChanged.length; i++) {
-  //           if (i === 0) { // 0 est de type ExportedNamedExpressionChange
-  //             const cell = cellChanged[i] as ExportedNamedExpressionChange;
-  //             this.data[idx].valeur = Number(cell.newValue);
-  //           } else if (i !== 1) { // On ne prend pas en compte indice 1 car c'est lui même comme 0, et le reste est de type ExportedCellChange
-  //             const cell = cellChanged[i] as ExportedCellChange;
-  //             this.data[cell.row].valeur = Number(cell.newValue);
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  onChangeFormula(event: Event, idx: number) {
+    const element = event.target;
+    if (element !== null) {
+      const selectedCode = this.data[idx].code;
+      const newFormula = (element as HTMLInputElement).value;
+      const affectedCells = this.hf.changeNamedExpression(selectedCode, `=${newFormula}`);
+      // Mise à jour les nouvelles valeurs affectées par le changement
+      affectedCells.forEach((cell) => {
+        const updatedCode = (cell as ExportedNamedExpressionChange).name;
+        const updatedValue = (cell as ExportedNamedExpressionChange).newValue;
 
-  // addNewRow() {
-  //   this.data.push({ code: '', formule: '', valeur: 0 });
-  //   this.tableDataSource = new MatTableDataSource(this.data);
-  //   this.table.renderRows();
-  // }
+        const dataItem = this.data.find((item) => item.code === updatedCode);
+        if (dataItem) {
+          dataItem.valeur = Number(updatedValue);
+        }
+      });
+    }
+  }
+
+  startEditCode(code: any) {
+    this.previousCodeValue = code;
+  }
+
+  startEditFormula(formule: any) {
+    this.previousCodeValue = formule;
+  }
+
+  deleteRow(idx: number) {
+    const selectedCode = this.data[idx].code;
+    const formula = this.data[idx].formule;
+    const changes = this.hf.removeNamedExpression(selectedCode);
+    let error = false;
+
+    for (let i = 0; i < changes.length; i++) {
+      const updatedValue = (changes[i] as ExportedNamedExpressionChange).newValue;
+      if (updatedValue instanceof DetailedCellError) {
+        error = true;
+        break;
+      }
+    };
+
+    if (!error) {
+      this.data.splice(idx, 1);
+      this.table.renderRows();
+    } else {
+      this.hf.addNamedExpression(selectedCode, `=${formula}`);
+      alert('Impossible de supprimer la ligne car ce code est utilisé dans la formule d\'une autre variable');
+    }
+  }
+
+  addNewRow() {
+    this.data.push({ code: '', formule: '', valeur: 0 });
+    this.table.renderRows();
+  }
 }
